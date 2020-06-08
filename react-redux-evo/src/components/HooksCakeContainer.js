c0f366106b16196eab3b402c21984984d18d4720
ciamac <hgzguzg>
2020-06-08 14:17:38 +0200

    Verified by Cia v1.1.2 use selector hook

---
+++ react-redux-evo/src/components/HooksCakeContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useSelector} from "react-redux";
+function HooksCakeContainer() {
+     const numOfCakes = useSelector(state => state.numOfCakes)
+      return (
+            <div>
+                  <h2>Num of cake -{numOfCakes} </h2>
+                  <button>Buy cake </button>
+            </div>
+      )
+}
+
+export default HooksCakeContainer;
