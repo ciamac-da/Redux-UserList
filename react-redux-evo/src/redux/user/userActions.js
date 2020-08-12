63e6d4a8d1db41578dd49182419296c8b2584e74
the-last-martian <the-last-martian@martian.localdomain>
2020-08-12 15:28:47 +0200

    Verified by Cia

--- react-redux-evo/src/redux/user/userActions.js
+++ react-redux-evo/src/redux/user/userActions.js
@@ -1,43 +1,42 @@
-import axios from "axios";
-import { FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS, FETCH_USERS_FAILURE } from "./userTypes";
-
-export const fetchUsersRequest = () => {
-      return {
-            type: FETCH_USERS_REQUEST
-      }
-}
-
-const fetchUsersSuccess = users => {
-      return{
-            type: FETCH_USERS_SUCCESS,
-            payload: users
-      }
-}
-
-const fetchUsersFailure = error => {
-      return{
-            type: FETCH_USERS_FAILURE,
-            payload: error
-      }
-}
-
+import axios from 'axios'
+import {
+  FETCH_USERS_REQUEST,
+  FETCH_USERS_SUCCESS,
+  FETCH_USERS_FAILURE
+} from './userTypes'
 
 export const fetchUsers = () => {
-      return function(dispatch) {
-            //this sets loading to true!
-            dispatch(fetchUsersRequest())
-            axios.get("https://jsonplaceholder.typicode.com/users")
-            .then( response =>{
-                  // response.data is the array description
-                  const users = response.data.map(user => user.id)
-                  dispatch(fetchUsersSuccess(users))
-                  console.log("hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii" + users )
-            })
-            .catch(error => {
-                  // error message is the description
-                  const errorMsg = error.message
-                  dispatch(fetchUsersFailure(errorMsg))
-            })
-      }
-      console.log("hoooooooooooooooooooooooi" +fetchUsers())
+  return (dispatch) => {
+    dispatch(fetchUsersRequest())
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        // response.data is the users
+        const users = response.data
+        dispatch(fetchUsersSuccess(users))
+      })
+      .catch(error => {
+        // error.message is the error message
+        dispatch(fetchUsersFailure(error.message))
+      })
+  }
+}
+
+export const fetchUsersRequest = () => {
+  return {
+    type: FETCH_USERS_REQUEST
+  }
+}
+
+export const fetchUsersSuccess = users => {
+  return {
+    type: FETCH_USERS_SUCCESS,
+    payload: users
+  }
+}
+
+export const fetchUsersFailure = error => {
+  return {
+    type: FETCH_USERS_FAILURE,
+    payload: error
+  }
 }
