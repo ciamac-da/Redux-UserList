58b5027cb93e255f4e7027aabf813be434e7affd
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-11 12:58:42 +0100

    Updates

---
+++ precache-manifest.3ad708cc65a74b279888d155f790e7bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b47f19721f9a2f6bc8e4672bbe4fea0",
+    "url": "/react-redux-userlist/index.html"
+  },
+  {
+    "revision": "d8d318af5cc4d14cd2cd",
+    "url": "/react-redux-userlist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "688eb3aeed6c5bc862bf",
+    "url": "/react-redux-userlist/static/js/2.97da1da4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-userlist/static/js/2.97da1da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8d318af5cc4d14cd2cd",
+    "url": "/react-redux-userlist/static/js/main.4722c41f.chunk.js"
+  },
+  {
+    "revision": "d15403860b171514f179",
+    "url": "/react-redux-userlist/static/js/runtime-main.d5badb71.js"
+  }
+]);
